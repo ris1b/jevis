76f4b3215485e6109e536ca6e13acda7f23e3bd1
ris1b <rishabjiwani44@gmail.com>
2026-05-17 23:41:51 +0530

    tcp echo server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+
+	"github.com/ris1b/jevis/config"
+	"github.com/ris1b/jevis/server"
+)
 
 func main() {
-	fmt.Println("Running Jevis DB ...")
+	setupFlags()
+	log.Println("Jevis on a roll")
+
+	server.RunSyncTCPServer()
+}
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the jevis server")
+	flag.IntVar(&config.Port, "port", 7379, "port for the jevis server")
+	flag.Parse()
 }
